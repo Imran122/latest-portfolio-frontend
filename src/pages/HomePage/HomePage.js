d687b61bb8c3c659d2007b33141c450d8da73836
imranhosain6554 <imran122.office@gmail.com>
2023-11-15 18:25:23 +0600

    fix4

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,19 +3,19 @@ import RightSidebar from "@/components/Shared/RightSidebar";
 
 const HomePage = () => {
   return (
-    <div className="grid lg:grid-cols-12 px-3 lg:px-0">
+    <div className="grid lg:grid-cols-10 px-3 lg:px-0">
       {/* <div className="relative">
         <div className="lg:col-span-3 border border-secondary_colour rounded-sm lg:sticky lg:h-[840px] lg:top-1/2 lg:-translate-y-1/2 lg:left-2">
           <LeftSidebar></LeftSidebar>
         </div>
       </div> */}
-      <div className="lg:h-screen lg:col-span-3 lg:sticky lg:top-0 flex items-center lg:left-2">
+      <div className="lg:h-screen lg:col-span-2 lg:sticky lg:top-0 flex items-center lg:left-2">
         <div className="border border-secondary_colour w-full">
           <LeftSidebar></LeftSidebar>
         </div>
       </div>
 
-      <div className="lg:col-span-8 lg:max-w-5xl mx-auto border lg:mt-0 sm:mt-6 mt-6">
+      <div className="lg:col-span-7 lg:max-w-6xl mx-auto border lg:mt-0 sm:mt-6 mt-6">
         <p>
           Since beginning my journey as a freelance designer nearly 8 years ago,
           done rem remote work for agencies, consulted for startups, and
